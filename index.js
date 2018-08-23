7728bbe22df049e198e649acffd893f2eb62d52e
Halljamie18 <jamie.hall@blis.com>
2018-08-23 11:22:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function updateDriverWithKeyAndValue (driver, key, value) {
 
 
 function updateDriverWithKeyAndValue (driver, key, value) {
-  driver[key] = value
+  driver[key] = value;
   return driver;
 }
 
